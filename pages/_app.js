405c5438caaa5147ec82d77847d55f9e3e6c97f3
Ashaffah <ashaffarafi@gmail.com>
2022-06-02 20:52:44 +0700

    fix huge fontawesomeicon onload page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { Component } from "react";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+
+config.autoAddCss = false;
+class MyApp extends Component {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
 }
 
-export default MyApp
+export default MyApp;
